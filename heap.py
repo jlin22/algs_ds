02ab50aad25f17952c8bb70ed7d722ab1516bfc6
John Lin <jlin1674@gmail.com>
2018-12-18 13:59:33 -0500

    finished heapsort

--- heap.py
+++ heap.py
@@ -3,9 +3,10 @@ def swap(array, i, j):
     array[i] = array[j]
     array[j] = temp
 
-def bubble_down(array, i, length):
+def bubble_down(array, i):
     ''' Bubble down in a min_heap '''
     # no children
+    length = len(array)
     if 2 * i >= length:
         return 
     # left exists, but right doesn't
@@ -19,32 +20,37 @@ def bubble_down(array, i, length):
         minimum = (2 * i) if (array[2 * i] < array[2 * i + 1]) else (2 * i + 1)
         if array[minimum] < array[i]: 
             swap(array, minimum, i)
-            return bubble_down(array, minimum, length)
+            return bubble_down(array, minimum)
         else:
             return
 
-def extract_min(array):
+def extract_min(array, length):
     ''' Extract min of a min heap '''
-    pass
+    swap(array, 1, length - 1)
+    minimum = array.pop()
+    bubble_down(array, 1)
+    return minimum
 
 def heap_sort(array, debug=False):
     # create our heap
     n = len(array)
     array.insert(0, 0)
     for i in reversed(range(1, n // 2 + 1)):
-        bubble_down(array, i, n + 1)
+        bubble_down(array, i)
 
     # create our sorted array by extract_max
     s = []
     for i in range(n):
-        s.append(extract_min(array))
+        s.append(extract_min(array, len(array)))
 
     return s
 
 array = [1, 45, 90, 66, 64, 43, 23, 12]
-heap_sort(array)
+sorted_array = heap_sort(array)
 print(array)
+print(sorted_array)
 
 array2 = [1, 45, 90, 66, 64, 43, 23, 12, 55]
 heap_sort(array2)
 print(array2)
+print(sorted_array)
