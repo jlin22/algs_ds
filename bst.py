955357e324d894556a031d4860c1d465e78c3175
John Lin <jlin1674@gmail.com>
2018-12-18 15:10:18 -0500

    bst comments

--- bst.py
+++ bst.py
@@ -1,12 +1,13 @@
 class Node:
+    # a node in a BinarySearchTree
     left = None
     right = None
-    parent = None     
 
     def __init__(self, value):
         self.value = value
 
 def insert_helper(node, prev, key):
+    # recursive function that inserts key into the BST
     if node == None: 
         if key < prev.value:
             prev.left = Node(key)
@@ -19,27 +20,39 @@ def insert_helper(node, prev, key):
             insert_helper(node.right, node, key)
     
 def inorder_helper(node):
+    # inorder printing of the BST
     if node != None:
         inorder_helper(node.left)
         print(node.value)
         inorder_helper(node.right)
+
+def inorder_save(node, stack):
+    # creates a sorted array of the elements of the tree w/ root=node
+    if node != None:
+        inorder_save(node.left, stack)
+        stack.append(node.value)
+        inorder_save(node.right, stack)
 
 class BinarySearchTree:
     def __init__(self):
         self.root = None
 
     def insert(self, value):
+        # insert an element into the BST
         if self.root == None:
             self.root = Node(value)
         else:
             insert_helper(self.root, None, value)
 
     def inorder(self):
-        inorder_helper(self.root)
+        # create a list of the elements in the BST in order
+        stack = []
+        inorder_save(self.root, stack)
+        return stack
 
 bst = BinarySearchTree()
 for i in range(10):
     bst.insert(i)
 bst.insert(-1)
 bst.insert(-3)
-bst.inorder()
+print(bst.inorder())
