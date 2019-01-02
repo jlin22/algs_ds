bcdad81a9e16e4133a16c98f009368df36c94da4
John Lin <jlin1674@gmail.com>
2019-01-01 21:06:42 -0500

    finished and parent

--- python/graphs/dfs.py
+++ python/graphs/dfs.py
@@ -1,17 +1,22 @@
 from graphs import Graph
 
-def dfs_visit(graph, source, parent):
+def dfs_visit(graph, source, parent, finished):
     for u in graph.adj_list(source):
         if u not in parent:
             parent[u] = source
-            dfs_visit(graph, u, parent)
+            dfs_visit(graph, u, parent, finished)
+    finished.append(source)
 
 def dfs(graph):
     parent = {}
+    finished = []
     vertices = graph.vertices()
     for v in vertices:
         if v not in parent:
-            dfs_visit(graph, v, parent)
+            parent[v] = None
+            dfs_visit(graph, v, parent, finished)
+    finished = list(reversed(finished))
+    return parent, finished
     
 if __name__ == '__main__':
     g = Graph()
@@ -21,4 +26,4 @@ if __name__ == '__main__':
     g.add_edge(2, 3)
     g.add_edge(3, 4)
     g.add_edge(4, 1)
-    dfs(g)
+    print(dfs(g))
